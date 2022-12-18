723b2a351aa1f842b53b27fa19f3c7b2bfca8c85
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-19 01:55:58 +0200

    Add "fetchPosts" fetching reducers

--- src/features/posts/posts-slice.tsx
+++ src/features/posts/posts-slice.tsx
@@ -1,9 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchPosts } from '@features/posts';
+
+const initialState = { data: [], isLoading: false, error: null };
 
 const postsSlice = createSlice({
   name: 'posts',
-  initialState: { data: [], loading: false, error: null },
+  initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchPosts.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchPosts.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(fetchPosts.rejected, (state, action) => {
+      state.isLoading = false;
+      state.data = [];
+      state.error = action.error.message;
+      // FIX: TS SetUP NEEDED
+    });
+  },
 });
 
-export const postsReducer = postsSlice.reducer
+export const postsReducer = postsSlice.reducer;
