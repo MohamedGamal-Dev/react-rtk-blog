2ac3ae9ea0ea095956b440198125985d2a7941dc
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-20 13:53:05 +0200

    Add absolute path support to vite
    Add two solutions
    - Manual resolve
    - Auto using 'vite-tsconfig-paths' plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // ==> DEV <==
+  // MANUAL ABSOLUTE PATH SUPPORT
+  // Use if there's an issue with [vite-tsconfig-paths] plugin
+  // ----------------------------------------------------------
+  // resolve: {
+  //   alias: [
+  //     { find: '@/*', replacement: 'src/*' },
+  //   ],
+  // },
+  plugins: [tsconfigPaths(), react()],
+});
