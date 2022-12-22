f7a132ecfcd30db5a94da11c6412e2663df6131c
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-22 03:06:36 +0200

    Add App Initial "fetchUsers" setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useEffect } from 'react';
 
 import { useDispatch } from '@utils/use-typed-rtk-store';
 import { PostsList, fetchPosts } from '@/features/posts';
-
+import { fetchUsers } from '@/features/users';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchUsers());
   }, []);
 
   return (
