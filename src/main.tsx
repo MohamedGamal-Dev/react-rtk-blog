2f35f90ec43ed0725cb8b64086a487858066077c
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-22 04:55:52 +0200

    Add "APP" routes setup in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import { store } from '@/app/store';
 import App from './App';
@@ -9,7 +10,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
