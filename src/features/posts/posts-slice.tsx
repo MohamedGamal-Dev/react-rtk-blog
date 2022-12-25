282d456de60bb4b617ab1f25b361909d3d1f9458
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-25 16:18:43 +0200

    Add "post/delete" reducers

--- src/features/posts/posts-slice.tsx
+++ src/features/posts/posts-slice.tsx
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addPost, fetchPosts } from '@/features/posts';
+import { addPost, deletePost, fetchPosts } from '@/features/posts';
 import { PostsState } from '@/types/posts';
 
 const initialState: PostsState = { data: [], isLoading: false, error: null };
@@ -9,30 +9,45 @@ const postsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(fetchPosts.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(fetchPosts.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.data = action.payload;
-    });
-    builder.addCase(fetchPosts.rejected, (state, action) => {
-      state.isLoading = false;
-      state.data = [];
-      state.error = action.error.message!;
-    });
-    
-    builder.addCase(addPost.pending, (state, action) => {
-      state.isLoading = true;
-    });
-    builder.addCase(addPost.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.data.push(action.payload);
-    });
-    builder.addCase(addPost.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.error.message!;
-    });
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.data = [];
+        state.error = action.error.message!;
+      });
+
+    builder
+      .addCase(addPost.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(addPost.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data.push(action.payload);
+      })
+      .addCase(addPost.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message!;
+      });
+
+    builder
+      .addCase(deletePost.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = state.data.filter((post) => post.id !== action.payload.id);
+      })
+      .addCase(deletePost.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message!;
+      });
   },
 });
 
