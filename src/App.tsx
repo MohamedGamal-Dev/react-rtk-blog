b2f9cb1a87f3cbe51ea7ac730795406583321385
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-26 22:36:46 +0200

    Add "post/edit" routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { useDispatch } from '@/app/use-typed-rtk-store';
-import { PostsList, PostAdd, fetchPosts } from '@/features/posts';
+import { fetchPosts, PostsList, PostAdd, PostEdit } from '@/features/posts';
 import { fetchUsers } from '@/features/users';
-import { HOME_PAGE, POST } from '@/routes';
+import { HOME_PAGE, POST, POST_EDIT } from '@/routes';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -20,6 +20,7 @@ const App = () => {
         <Route path={HOME_PAGE} element={<PostsList />} />
         <Route path={POST}>
           <Route index element={<PostAdd />} />
+          <Route path={POST_EDIT} element={<PostEdit />} />
         </Route>
       </Route>
     </Routes>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,13 @@
 export const HOME_PAGE = '/';
 export const POST = '/post';
 export const POST_ADD = '/post';
+export const POST_BY_ID = `/post/:postId`;
+export const POST_EDIT = `/post/:postId/edit`;
+
+export const postDynamicRoute = (id: string = ':postId') => {
+  return `/post/${id}`;
+};
+
+export const postEditDynamicRoute = (id: string = ':postId') => {
+  return `/post/${id}/edit`;
+};
