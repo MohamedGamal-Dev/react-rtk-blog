a16022846401f879be1da243e301729695db8fbf
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-29 10:05:00 +0200

    Create single post render component

---
+++ src/features/posts/post-page-render.component.tsx
@@ -0,0 +1,42 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { FaTrashAlt, FaEdit } from 'react-icons/fa';
+
+import { useDispatch } from '@/app/use-typed-rtk-store';
+import { useStoreState } from '@/app/use-store-state';
+import { HOME_PAGE, postEditDynamicRoute } from '@/routes';
+import { deletePost } from '@/features/posts';
+import { Button } from '@ui/forms';
+
+const PostPageRender = () => {
+  const dispatch = useDispatch();
+  const { postId } = useParams();
+  const navigate = useNavigate();
+  const { getPostById } = useStoreState();
+  const post = getPostById(postId!);
+  const { id, title, body } = post;
+
+  const handleDeleteClick = () => {
+    dispatch(deletePost(post));
+    navigate(HOME_PAGE);
+  };
+  const handleEditClick = () => {
+    navigate(postEditDynamicRoute(id));
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+
+      <Button primary onClick={handleEditClick}>
+        <FaEdit />
+      </Button>
+      <Button danger onClick={handleDeleteClick}>
+        <FaTrashAlt />
+      </Button>
+
+      <p>{body}</p>
+    </>
+  );
+};
+
+export default PostPageRender;
