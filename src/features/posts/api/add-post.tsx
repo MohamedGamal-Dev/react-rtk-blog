144a0fc7d979a2eefb0c5c2dcf627df2f14bcd1c
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-31 22:11:41 +0200

    Update "post/add" prepare with date & reactions

--- src/features/posts/api/add-post.tsx
+++ src/features/posts/api/add-post.tsx
@@ -1,5 +1,6 @@
 import { createAsyncThunk, nanoid } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { formatISO } from 'date-fns';
 
 import { BaseURL_DEV } from '@/routes/api';
 import { PostPrepare, PostType } from '@/types/posts';
@@ -7,9 +8,19 @@ import { PostPrepare, PostType } from '@/types/posts';
 export const addPost = createAsyncThunk(
   'post/add',
   async ({ userId, title, body }: PostPrepare) => {
-    const newPost: PostType = { userId, id: nanoid(), title, body };
+    const newPost: PostType = {
+      userId,
+      id: nanoid(),
+      title,
+      body,
+      date: formatISO(new Date()),
+      reactions: {
+        thumbsUp: 0,
+        thumbsDown: 0,
+      },
+    };
 
     const response = await axios.post(`${BaseURL_DEV}/posts`, newPost);
     return response.data;
   }
-);
+); 
