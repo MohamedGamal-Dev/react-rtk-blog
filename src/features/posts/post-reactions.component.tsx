5cebbfd0200b85f8a86289d701e1c635498c317a
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2023-01-03 21:16:02 +0200

    Edit 'post-reactions' & add reactionsCount action

--- src/features/posts/post-reactions.component.tsx
+++ src/features/posts/post-reactions.component.tsx
@@ -1,32 +1,38 @@
+
 import { FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
 
 import { PostProps } from '@/types/posts';
 import { useDispatch } from '@/app/use-typed-rtk-store';
 import { Button } from '@ui/forms';
+import { reactionCount } from '@/features/posts';
 
 const PostReactions = ({ post }: PostProps) => {
   const dispatch = useDispatch();
+  const { id, reactions } = post!;
 
   let reactionsIcons = {
     thumbsUp: <FaThumbsUp />,
     thumbsDown: <FaThumbsDown />,
   };
 
-  return (
-    <>
-      {Object.entries(reactionsIcons).map(([reaction, icon]) => {
-        return (
-          <Button
-            key={reaction}
-          >
-            {icon}
+  const renderReactions = Object.entries(reactionsIcons).map(
+    ([reaction, icon]) => {
+      return (
+        <button
+          key={reaction}
+          onClick={() => {
+            dispatch(reactionCount({ id, reaction }));
+          }}
+        >
+          {icon}
 
-            <div>{reactions![reaction]}</div>
-          </Button>
-        );
-      })}
-    </>
+          <div>{reactions![reaction]}</div>
+        </button>
+      );
+    }
   );
+
+  return <>{renderReactions}</>;
 };
 
 export default PostReactions;
