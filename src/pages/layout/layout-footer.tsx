66598152151d0b23546fb8a5ecd3310942bc21bc
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2023-01-08 23:04:22 +0200

    Add "layout/footer" Initial setup

--- src/pages/layout/layout-footer.tsx
+++ src/pages/layout/layout-footer.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { footerCopyright } from './consts';
 
 export const LayoutFooter = () => {
+  const { year, linkUrl, linkLabel, copyRight } = footerCopyright;
   return (
-    <div>LayoutFooter</div>
-  )
-}
+    <>
+      <footer className="bg-dark-primary p-4 shadow md:flex md:items-center md:justify-between md:p-6">
+        <span className="text-sm text-light-off sm:text-center">
+          {year}
+          <a href={linkUrl} className="hover:underline">
+            {linkLabel}
+          </a>
+          {copyRight}
+        </span>
+      </footer>
+    </>
+  );
+};
