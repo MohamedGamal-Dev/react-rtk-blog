ccec3923d5256aa69f0ce094711261cd90a3d7ec
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2023-01-09 13:41:58 +0200

    Edit "posts/list" render setup and betaStyles
    - Edit render to show latest post first
    - Add new Styles

--- src/features/posts/posts-list.component.tsx
+++ src/features/posts/posts-list.component.tsx
@@ -3,25 +3,31 @@ import { Fragment } from 'react';
 import { useSelector } from '@/app/use-typed-rtk-store';
 import { PostType } from '@/types/posts';
 import { PostItem } from '@/features/posts';
+import { Loader } from '@/features/ui';
 
 const PostsList = () => {
   const { data, isLoading, error } = useSelector((state) => {
     return state.posts;
   });
 
-  const renderList = () => {
-    return data.map((post: PostType) => (
-      <Fragment key={post.id}>
-        <PostItem post={post} />
-      </Fragment>
-    ));
+  const renderReverseList = (data: PostType[]) => {
+    let postsList = [];
+    for (let i = data.length - 1; i >= 0; i--) {
+      const post = data[i];
+      postsList.push(
+        <Fragment key={post.id}>
+          <PostItem post={post} />
+        </Fragment>
+      );
+    }
+    return postsList;
   };
 
   return (
-    <div>
-      {isLoading && <h1>Loading...</h1>}
+    <div className="flex flex-row flex-wrap justify-center ">
+      {isLoading && <Loader />}
       {error && <p>{error}</p>}
-      {renderList()}
+      {renderReverseList(data)}
     </div>
   );
 };
