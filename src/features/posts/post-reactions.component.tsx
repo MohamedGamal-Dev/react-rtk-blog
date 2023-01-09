7fb42a238166a54b00798f775de39ac66f67d665
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2023-01-09 13:43:46 +0200

    Add "post/reactions" beta styles

--- src/features/posts/post-reactions.component.tsx
+++ src/features/posts/post-reactions.component.tsx
@@ -22,10 +22,13 @@ const PostReactions = ({ post }: PostProps) => {
           onClick={() => {
             dispatch(reactionsCount({ post, reaction }));
           }}
+          className="flex flex-row items-center space-x-2 overflow-hidden rounded-lg bg-primary-main  py-1.5 pl-2 pr-1 text-light-primary  hover:bg-secondary-main focus:outline-none"
         >
           {icon}
 
-          <div>{reactions![reaction]}</div>
+          <div className="rounded-lg bg-light-primary px-3.5 py-0 font-medium text-dark-primary ">
+            {reactions![reaction]}
+          </div>
         </button>
       );
     }
