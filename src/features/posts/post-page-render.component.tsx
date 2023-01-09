2d57a4e09c0fed9728eac1b47a3ff742343711f2
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2023-01-09 13:45:18 +0200

    Add "post/single-page-render" beta styles

--- src/features/posts/post-page-render.component.tsx
+++ src/features/posts/post-page-render.component.tsx
@@ -4,7 +4,12 @@ import { FaTrashAlt, FaEdit } from 'react-icons/fa';
 import { useDispatch } from '@/app/use-typed-rtk-store';
 import { useStoreState } from '@/app/use-store-state';
 import { HOME_PAGE, postEditDynamicRoute } from '@/routes';
-import { deletePost, PostReactions } from '@/features/posts';
+import {
+  deletePost,
+  PostAuthor,
+  PostDate,
+  PostReactions,
+} from '@/features/posts';
 import { Button } from '@ui/forms';
 
 const PostPageRender = () => {
@@ -13,7 +18,7 @@ const PostPageRender = () => {
   const navigate = useNavigate();
   const { getPostById } = useStoreState();
   const post = getPostById(postId!);
-  const { id, title, body } = post;
+  const { userId, id, title, body, date, image } = post;
 
   const handleDeleteClick = () => {
     dispatch(deletePost(post));
@@ -24,20 +29,53 @@ const PostPageRender = () => {
   };
 
   return (
-    <>
-      <h2>{title}</h2>
+    <div className="bg-dark-off border border-dark-primary rounded-lg shadow-md ">
+      {image ? (
+        <img src={image!.src} alt={image!.alt} className="rounded-t-lg" />
+      ) : null}
 
-      <Button primary onClick={handleEditClick}>
-        <FaEdit />
-      </Button>
-      <Button danger onClick={handleDeleteClick}>
-        <FaTrashAlt />
-      </Button>
+      <div className="p-5">
+        <div className="flex items-center justify-between">
+          <h2 className="mb-2 text-2xl font-bold tracking-tight text-white">
+            {title}
+          </h2>
 
-      <p>{body}</p>
+          <div className="flex items-center space-x-1">
+            <button
+              className="transform cursor-pointer rounded bg-primary-dark px-3 py-2 font-bold text-white transition-colors duration-200 hover:bg-secondary-dark hover:text-dark-primary"
+              onClick={handleEditClick}
+            >
+              <FaEdit />
+            </button>
 
-      <PostReactions post={post!} />
-    </>
+            <Button
+              className="transform cursor-pointer rounded bg-danger-dark px-3 py-2 font-bold text-white transition-colors duration-200 hover:bg-light-secondary hover:text-danger-dark"
+              onClick={handleDeleteClick}
+            >
+              <FaTrashAlt />
+            </Button>
+          </div>
+        </div>
+
+        <div className="flex items-start space-x-2 border-b-2 border-divider-dark pb-1">
+          <div className="font-serif text-sm font-medium text-secondary-dark">
+            <PostAuthor userId={userId} />
+          </div>
+
+          <div className="text-sm font-light text-light-secondary">
+            <PostDate timestamp={date} />
+          </div>
+        </div>
+
+        <p className="py-3 font-normal text-light-off">{body}</p>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-1">
+            <PostReactions post={post!} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
