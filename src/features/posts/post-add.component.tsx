163545585b0472bef558b9a9020bfd4f648ae4b4
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2023-01-09 13:46:34 +0200

    Add "post/add" beta styles

--- src/features/posts/post-add.component.tsx
+++ src/features/posts/post-add.component.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch } from '@/app/use-typed-rtk-store';
 import { addPost } from '@/features/posts';
 import { useStoreState } from '@/app/use-store-state';
+import { Input, TextareaFiled, Button, Container } from '@/features/ui';
 
 const PostAdd = () => {
   const dispatch = useDispatch();
@@ -51,7 +52,12 @@ const PostAdd = () => {
     return users.map((user) => {
       return (
         <React.Fragment key={user.id}>
-          <option value={user.id}>{user.name}</option>
+          <option
+            value={user.id}
+            className="bg-background-dark text-lg  text-light-primary"
+          >
+            {user.name}
+          </option>
         </React.Fragment>
       );
     });
@@ -59,43 +65,51 @@ const PostAdd = () => {
 
   return (
     <>
-      <div>
-        <div>
-          <form
-            onSubmit={handleFormSubmit}
-            className="flex flex-col space-y-4 bg-slate-400"
-          >
-            <input
+      <Container center>
+        <div className=" bg-dark-off border border-dark-primary rounded-lg shadow-md p-4">
+          <form onSubmit={handleFormSubmit} className="flex flex-col space-y-4">
+            <Input
               name="title"
               value={title}
               onChange={handleOnChange}
               placeholder="Please Enter Post Title"
               required
+              className="block w-full rounded-lg border-2 border-primary-main bg-transparent p-2.5 text-lg text-light-primary shadow-sm shadow-primary-main outline-none focus:border-secondary-main focus:shadow-secondary-main"
             />
 
-            <textarea
+            <TextareaFiled
               name="body"
               value={body}
               onChange={handleOnChange}
               rows={4}
               placeholder="Please Enter Post Content"
               required
-            ></textarea>
+              className="block w-full rounded-lg border-2 border-primary-main bg-transparent p-2.5 text-lg text-light-primary shadow-sm shadow-primary-main outline-none focus:border-secondary-main focus:shadow-secondary-main"
+            ></TextareaFiled>
 
             <select
               name="userId"
               value={userId}
               onChange={handleOnChange}
               id="author"
+              className="block w-full rounded-lg border-2 border-primary-main bg-transparent p-2.5 text-lg text-light-primary shadow-sm shadow-primary-main outline-none focus:border-secondary-main focus:shadow-secondary-main"
             >
-              <option value=""> Please choose author </option>
+              <option
+                value=""
+                className="bg-background-dark text-lg  text-light-primary"
+              >
+                {' '}
+                Please choose author{' '}
+              </option>
               {renderUsersOptionList()}
             </select>
 
-            <button>Submit</button>
+            <Button className="rounded-lg bg-primary-main px-5 py-2.5 text-center text-lg font-bold text-white shadow-sm shadow-dark-secondary hover:bg-secondary-dark hover:shadow-warning-light focus:outline-none">
+              Submit
+            </Button>
           </form>
         </div>
-      </div>
+      </Container>
     </>
   );
 };
